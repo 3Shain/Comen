43844552d06388c6868ee1cf95ddda7abea12893
3Shain <san3shain@outlook.com>
2019-05-02 20:08:00 +0800

    update environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   official: false,
   production: true,
-  api_server: '/api' // 编译docker镜像时请修改此字段
+  api_server: '/api', // 编译docker镜像时请修改此字段
+  default_avatar: 'http://static.hdslb.com/images/member/noface.gif'
 };
