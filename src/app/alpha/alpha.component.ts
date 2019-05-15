a9a11ba4ec22a1581fc93ef225214fc82f293296
3Shain <san3shain@outlook.com>
2019-05-15 12:17:30 +0800

    second i18n update

--- src/app/alpha/alpha.component.ts
+++ src/app/alpha/alpha.component.ts
@@ -6,6 +6,7 @@ import { ChatRendererComponent } from './chat-renderer/chat-renderer.component';
 import { BiliwsService } from '../biliws.service';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-alpha',
@@ -24,11 +25,14 @@ export class AlphaComponent implements OnInit {
     private title: Title,
     private proc: MessageProcessorService,
     private bili: BiliwsService,
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private translate: TranslateService) { }
 
   ngOnInit() {
     this.currentRoomId = this.route.snapshot.params['id'];
-    this.title.setTitle('直播间' + this.currentRoomId);
+    this.translate.get('ROOM').subscribe((value) => {
+      this.title.setTitle(value + ' ' + this.currentRoomId);
+    });
 
     if (this.route.snapshot.queryParamMap.has('loadAvatar')) {
       this.proc.loadAvatar = this.route.snapshot.queryParamMap.get('loadAvatar').toLowerCase() === 'true';
@@ -55,40 +59,50 @@ export class AlphaComponent implements OnInit {
 
   onload() {
     if (this.currentRoomId <= 0) {
-      this.renderer.sendSystemInfo('直播间ID格式错误');
+      this.translate.get('IDFORMATERROR').subscribe((value) => {
+        this.renderer.sendSystemInfo(value);
+      });
       return;
     }
-    this.renderer.sendSystemInfo('正在获取直播间信息...');
+    this.translate.get('GETROOMINFO').subscribe((value) => {
+      this.renderer.sendSystemInfo(value);
+    });
     this.http.get(`${environment.api_server}/stat/${this.currentRoomId}`).subscribe(
       (x: any) => {
         this.bili.ownerId = x.uid;
         if (x.config) {
-          this.proc.loadAvatar = x.config.loadAvatar||this.proc.loadAvatar;
-          this.proc.userLevelFilter = x.config.levelFilter||this.proc.userLevelFilter;
-          this.proc.hideGiftDanmaku = x.config.hideGiftDanmaku||this.proc.hideGiftDanmaku;
-          this.proc.showGift = x.config.showGift||this.proc.showGift;
-          this.proc.wordFilter = this.proc.wordFilter.concat(x.config.wordFilter||[]);
-          this.proc.customEmotions = x.config.customEmotions||[];
-          this.renderer.displayMode = x.config.displayMode||this.renderer.displayMode;
-          this.renderer.groupSimilar = x.config.groupSimilar||this.renderer.groupSimilar;
-          this.renderer.groupSimilarWindow = x.config.groupSimilarWindow||this.renderer.groupSimilarWindow;
-          this.renderer.maxDammakuNum = x.config.maxDammakuNumber||this.renderer.maxDammakuNum;
+          this.proc.loadAvatar = x.config.loadAvatar || this.proc.loadAvatar;
+          this.proc.userLevelFilter = x.config.levelFilter || this.proc.userLevelFilter;
+          this.proc.hideGiftDanmaku = x.config.hideGiftDanmaku || this.proc.hideGiftDanmaku;
+          this.proc.showGift = x.config.showGift || this.proc.showGift;
+          this.proc.wordFilter = this.proc.wordFilter.concat(x.config.wordFilter || []);
+          this.proc.customEmotions = x.config.customEmotions || [];
+          this.renderer.displayMode = x.config.displayMode || this.renderer.displayMode;
+          this.renderer.groupSimilar = x.config.groupSimilar || this.renderer.groupSimilar;
+          this.renderer.groupSimilarWindow = x.config.groupSimilarWindow || this.renderer.groupSimilarWindow;
+          this.renderer.maxDammakuNum = x.config.maxDammakuNumber || this.renderer.maxDammakuNum;
         }
         this.start(x.room_id);
       },
       e => {
-        this.renderer.sendSystemInfo('直播间信息获取失败,尝试rawId');
+        this.translate.get('ROOMINFORAWID').subscribe((value) => {
+          this.renderer.sendSystemInfo(value);
+        });
         this.start(this.currentRoomId);
       }
     );
   }
 
   start(realRoomId: number) {
-    this.renderer.sendSystemInfo(`正在连接到直播间${realRoomId}...`);
+    this.translate.get('CONNECTING').subscribe((value) => {
+      this.renderer.sendSystemInfo(value + realRoomId + '...');
+    });
     this.bili.connect(Number(realRoomId)).subscribe(
       message => {
         if (message.type === 'connected') {
-          this.renderer.sendSystemInfo('成功连接到直播间!');
+          this.translate.get('CONNECTED').subscribe((value) => {
+            this.renderer.sendSystemInfo(value);
+          });
           if (environment.official) {
             //this.renderer.sendSystemInfo('你正在使用公共服务器提供的服务，为了更高的稳定性，建议使用本地部署版本。详情访问https://bilichat.3shain.com');
           }
@@ -98,12 +112,16 @@ export class AlphaComponent implements OnInit {
       },
       e => {
         if (e.target.readyState === WebSocket.CLOSED) {
-          this.renderer.sendSystemInfo('无法连接到直播间,5秒后重试');
+          this.translate.get('CONNECTCLOSED').subscribe((value) => {
+            this.renderer.sendSystemInfo(value);
+          });
           setTimeout(() => this.start(realRoomId), 5000);
         }
       },
       () => {
-        this.renderer.sendSystemInfo('检测到服务器断开,尝试重连中...');
+        this.translate.get('DISCONNECTED').subscribe((value) => {
+          this.renderer.sendSystemInfo(value);
+        });
         this.start(realRoomId); // 重连
       }
     );
