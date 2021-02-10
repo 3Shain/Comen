5dca5201ab5dea3f7ca509730103d421b25ccf3f
3Shain <san3shain@outlook.com>
2021-02-10 19:55:20 +0800

    chore(core): add basic custom webpack config

---
+++ apps/core/webpack.config.ts
@@ -0,0 +1,18 @@
+import { CustomWebpackBrowserSchema, TargetOptions } from '@angular-builders/custom-webpack';
+import * as webpack from 'webpack';
+
+
+export default (
+  config: webpack.Configuration,
+  options: CustomWebpackBrowserSchema,
+  targetOptions: TargetOptions
+)=>{
+
+  config.plugins?.push(
+    new webpack.DefinePlugin({
+      
+    })
+  );
+
+  return config;
+}
